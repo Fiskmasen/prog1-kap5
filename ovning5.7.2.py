b02e881ffabe3ed77da49373fcf67c7684cb1c4f
Fiskmasen <70147767+Fiskmasen@users.noreply.github.com>
2021-10-22 10:16:07 +0200

    Koden fixad!
    Koden är äntligen fixad, hurra!

--- ovning5.7.2.py
+++ ovning5.7.2.py
@@ -5,17 +5,8 @@ persnum = input("Mata in ditt personnummer(yyyymmdd): ")
 persSplit = persnum.split("-")
 dtpers = dt.strptime(persSplit[0], '%Y%m%d')
 
-if (dt.month == dtpers.month and dt.day == dtpers.day):
+if dt.month == dtpers.month and dt.day == dtpers.day:
   print("Grattis det är din födelsedag! ", dtpers.date())
 
-if int(persSplit[1][-2]) % 2 == 0:
-  print("Du är en kvinna.")
 else:
-    print("Du är en man.")
-
-
-years = dt.year - dtpers.year
-months = dt.month - dtpers.month
-days = dt.day - dtpers.day
-
-print(f"Du är {years} år, {months} månader och {days} dagar gammal.")
+  print("Det är inte din födelsedag ännu, kom tillbaka när du faktiskt fyller år, ditt fä!")
